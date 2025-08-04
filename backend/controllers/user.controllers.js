4f49bcec62085cc3a2f22116dee5e0452e6252c2
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2025-08-04 13:35:07 +0300

    Add routes for admin to get, update, and delete users

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -113,4 +113,136 @@ const getUsers = asyncHandler(async (req, res) => {
   }
 });
 
-export { createUser, loginUser, logoutUser, getUsers };
+// Get current user profile
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select('-password');
+    if (!user) {
+      return res.status(404).json(fail({ message: 'User not found.' }));
+    }
+    return res.status(200).json(success({ user }));
+  } catch (e) {
+    return res.status(500).json(error(e.message, e.stack));
+  }
+});
+
+// Update user profile
+const updateUserProfile = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return res.status(404).json(fail({ message: 'User not found.' }));
+    }
+
+    const { username, email, password } = req.body;
+    if (username) user.username = username;
+    if (email) {
+      // Check if email is already used by another user
+      const existingUser = await User.findOne({ email });
+      if (existingUser && existingUser._id.toString() !== user._id.toString()) {
+        return res.status(400).json(fail({ message: 'Email already exists.' }));
+      }
+      user.email = email;
+    }
+
+    if (password) {
+      // Hash the new password
+      user.password = await bcrypt.hash(password, 10);
+    }
+    await user.save();
+    return res.status(200).json(
+      success({
+        message: 'User profile updated successfully',
+        user: {
+          username: user.username,
+          email: user.email,
+        },
+      })
+    );
+  } catch (e) {
+    res.status(500).json(error(e.message, e.stack));
+  }
+});
+
+// Delete user by ID (Admin only)
+const deleteUserById = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json(fail({ message: 'User not found.' }));
+    }
+
+    if (user.isAdmin) {
+      return res
+        .status(400)
+        .json(fail({ message: 'Cannot delete an admin user.' }));
+    }
+
+    await User.deleteOne({ _id: user._id });
+    return res
+      .status(200)
+      .json(success({ message: 'User deleted successfully.' }));
+  } catch (e) {
+    res.status(500).json(error(e.message, e.stack));
+  }
+});
+
+// Get user by ID (Admin only)
+const getUserById = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) {
+      return res.status(404).json(fail({ message: 'User not found.' }));
+    }
+    return res.status(200).json(success({ user }));
+  } catch (e) {
+    return res.status(500).json(error(e.message, e.stack));
+  }
+});
+
+// Update user by ID (Admin only)
+const updateUserById = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json(fail({ message: 'User not found.' }));
+    }
+    const { username, email, isAdmin } = req.body;
+    if (username) user.username = username;
+    if (email) {
+      // Check if email is already used by another user
+      const existingUser = await User.findOne({ email });
+      if (existingUser && existingUser._id.toString() !== user._id.toString()) {
+        return res.status(400).json(fail({ message: 'Email already exists.' }));
+      }
+      user.email = email;
+    }
+    if (isAdmin !== undefined) user.isAdmin = isAdmin;
+    await user.save();
+    return res.status(200).json(
+      success({
+        message: 'User updated successfully',
+        user: {
+          username: user.username,
+          email: user.email,
+          isAdmin: user.isAdmin,
+        },
+      })
+    );
+  } catch (e) {
+    return res.status(500).json(error(e.message, e.stack));
+  }
+});
+
+export {
+  createUser,
+  loginUser,
+  logoutUser,
+  getUsers,
+  getCurrentUserProfile,
+  updateUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+};
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -4,16 +4,32 @@ import {
   loginUser,
   logoutUser,
   getUsers,
+  getCurrentUserProfile,
+  updateUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
 } from '../controllers/user.controllers.js';
 import { authenticate, authAdmin } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-// Create a new user
 router.route('/').post(createUser).get(authenticate, authAdmin, getUsers);
 
 // Login a user
 router.post('/login', loginUser);
+
+// Get current user profile
+router
+  .route('/profile')
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateUserProfile);
+
+router
+  .route('/:id')
+  .delete(authenticate, authAdmin, deleteUserById)
+  .get(authenticate, authAdmin, getUserById)
+  .put(authenticate, authAdmin, updateUserById);
 
 // Logout a user
 router.post('/logout', logoutUser);
